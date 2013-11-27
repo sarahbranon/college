17e90b96df4d9e7334f6d3f93fcc53933039e5bc
Sarah Branon <sarahbranon@gmail.com>
2013-11-27 14:04:07 -0800

    Soft design.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,7 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, alert: exception.message
   end
 
+  def after_sign_in_path_for(resource)
+    home_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 College::Application.routes.draw do
-  root to: "home#index"
-  devise_for :users, controllers: {registrations: "registrations", omniauth_callbacks: "users/omniauth_callbacks"}
+  devise_for :users,
+    controllers: { registrations: "registrations", omniauth_callbacks: "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    root to: "registrations#new"
+  end
+
   resources :users do
     member do
       get "onboarding"
     end
   end
+
+  get "home/index", as: "home"
 end
